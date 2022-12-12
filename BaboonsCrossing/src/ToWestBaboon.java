92dc9366a56f44b7025a5461a16345b981ca6467
Malek Alismail <90925322+Malekabo123@users.noreply.github.com>
2022-12-12 23:04:16 +0300

    Update ToWestBaboon.java

--- BaboonsCrossing/src/ToWestBaboon.java
+++ BaboonsCrossing/src/ToWestBaboon.java
@@ -9,15 +9,42 @@ public class ToWestBaboon implements Runnable{
 
     @Override
     public void run(){
+
         while (true){
             try {
-                Thread.sleep((long) ((int) 5*Math.random()*1000));
-                crossingCanyon.acquireGoToWest();
-                System.out.println("I am baboon "+id+" going west");
+                Thread.sleep((long) (Math.random()*500));
 
-                Thread.sleep((long) ((int) 5*Math.random()*1000));
-                crossingCanyon.releaseGoToWest();
-                System.out.println("I am baboon "+id+" arrived west");
+                { //going to west
+                    crossingCanyon.blockingEntrance.acquire();
+                    crossingCanyon.toWestMutex.acquire();
+                    crossingCanyon.toWestCounter++;
+
+                    if (crossingCanyon.toWestCounter == 1){ //the first baboon crossing to west
+                        crossingCanyon.rope.acquire(); // the way is reserved as to west now
+                    }
+                    crossingCanyon.toWestMutex.release();
+                    crossingCanyon.blockingEntrance.release();
+
+
+                    crossingCanyon.toWestCount.acquire();
+                }
+
+                System.out.println("I am baboon "+id+" going west.\n");
+                Thread.sleep((long) (Math.random()*500));
+                System.out.println("I am baboon "+id+" arrived west.\n");
+
+                { //reached west
+                    crossingCanyon.toWestCount.release();
+                    crossingCanyon.toWestMutex.acquire();
+
+                    crossingCanyon.toWestCounter--;
+
+                    if (crossingCanyon.toWestCounter == 0) {
+                        crossingCanyon.rope.release(); //the way is empty now
+                    }
+
+                    crossingCanyon.toWestMutex.release();
+                }
 
             }catch (Exception e){
 
